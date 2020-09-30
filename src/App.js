d55e7fd838fbbb1ecc770e4d0c53fb26cde0d05e
Pan Tofel <66726051+pantek23@users.noreply.github.com>
2020-09-30 22:40:50 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/layout/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Address from "./components/pages/Address";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Switch>
+            <Route exact path="/address" component={Address} />
+          </Switch>
+          <Switch>
+            <Route exact path="/about" component={About} />
+          </Switch>
+          <Switch>
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
